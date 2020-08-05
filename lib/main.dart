e25f3f451253f470f1257b44f75b32200750f9de
Nikhil Sharma <nikhil.sk.sharma@gmail.com>
2020-08-05 20:41:42 +0530

    add provider and base theme

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import './screens/product_detail_screen.dart';
 import './screens/product_overview.dart';
+import 'package:google_fonts/google_fonts.dart';
+import './providers/products.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'MyShop',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+    return ChangeNotifierProvider(
+      create: (ctx) => Products(),
+          child: MaterialApp(
+        title: 'MyShop',
+        theme: ThemeData(
+            primarySwatch: Colors.pink,
+            accentColor: Colors.amberAccent,
+            fontFamily: GoogleFonts.lato().fontFamily),
+        home: ProductsOverview(),
+        routes: {
+          ProductDetailScreen.routeName : (ctx) => ProductDetailScreen()
+        },
       ),
-      home: ProductsOverview(),
     );
   }
 }
