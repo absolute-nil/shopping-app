ca3cf87a34f6af6b96a1bc75d40f29b92bea54ee
Nikhil Sharma <nikhil.sk.sharma@gmail.com>
2020-08-05 20:42:59 +0530

    make file leaner

--- lib/screens/product_overview.dart
+++ lib/screens/product_overview.dart
@@ -1,10 +1,8 @@
 import 'package:flutter/material.dart';
-import '../models/product.dart';
-import '../data/products.dart';
-import '../widgets/product_item.dart';
+
+import '../widgets/products_grid.dart';
 
 class ProductsOverview extends StatelessWidget {
-  final List<Product> loadedProducts = productsData;
 
   @override
   Widget build(BuildContext context) {
@@ -12,20 +10,8 @@ class ProductsOverview extends StatelessWidget {
       appBar: AppBar(
         title: Text("Shopping App"),
       ),
-      body: GridView.builder(
-          padding: EdgeInsets.all(10),
-          itemBuilder: (ctx, i) => ProductItem(
-              loadedProducts[i].id,
-              loadedProducts[i].title,
-              loadedProducts[i].description,
-              loadedProducts[i].price,
-              loadedProducts[i].imageUrl),
-          itemCount: loadedProducts.length,
-          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-              crossAxisCount: 2,
-              childAspectRatio: 3 / 2,
-              crossAxisSpacing: 10,
-              mainAxisSpacing: 10)),
+      body: ProductsGrid(),
     );
   }
 }
+
