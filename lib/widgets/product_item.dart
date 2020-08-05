040322e536f03d1a8be4eaf29f6cc2f9257c2f37
Nikhil Sharma <nikhil.sk.sharma@gmail.com>
2020-08-05 20:45:21 +0530

    display product item with fav and cart

--- lib/widgets/product_item.dart
+++ lib/widgets/product_item.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:shopping_app/screens/product_detail_screen.dart';
 
 class ProductItem extends StatelessWidget {
   final String id;
@@ -11,22 +12,80 @@ class ProductItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return GridTile(
-      child: Image.network(
-        imageUrl,
-        fit: BoxFit.cover,
+    return Container(
+      decoration: BoxDecoration(
+        shape: BoxShape.rectangle,
+        borderRadius: BorderRadius.circular(10),
+        color: Colors.white,
+        boxShadow: [
+          BoxShadow(
+              color: Colors.grey,
+              blurRadius: 1.0,
+              offset: Offset(1.0, 1.0),
+              spreadRadius: 2.0)
+        ],
       ),
-      footer: GridTileBar(
-        backgroundColor: Colors.black54,
-        leading: IconButton(
-          icon: Icon(Icons.favorite),
-          onPressed: () {},
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(10),
+        child: GridTile(
+          child: Stack(
+            children: <Widget>[
+              new Positioned.fill(child:
+              Image.network(
+                imageUrl,
+                fit: BoxFit.cover,
+              ) 
+              )
+              ,
+              new Positioned.fill(
+                child: Material(
+                  color: Colors.transparent,
+                  
+                  child: InkWell(
+                    splashColor: Theme.of(context).accentColor.withOpacity(0.4),
+
+                    borderRadius: BorderRadius.circular(10),
+                    onTap: () {
+                      Navigator.of(context).pushNamed(
+                        ProductDetailScreen.routeName,
+                        arguments: id,
+                      );
+                    },
+                  ),
+                ),
+              )
+            ],
+          ),
+          // child: GestureDetector(
+          //   onTap: () {
+          //     Navigator.of(context).pushNamed(
+          //       ProductDetailScreen.routeName,
+          //       arguments: id,
+          //     );
+          //   },
+          //   child: Image.network(
+          //     imageUrl,
+          //     fit: BoxFit.cover,
+          //   ),
+          // ),
+          footer: GridTileBar(
+            backgroundColor: Colors.black87,
+            leading: IconButton(
+              icon: Icon(Icons.favorite),
+              onPressed: () {},
+              color: Theme.of(context).accentColor,
+            ),
+            title: Text(
+              title,
+              textAlign: TextAlign.center,
+            ),
+            trailing: IconButton(
+              icon: Icon(Icons.shopping_cart),
+              onPressed: () {},
+              color: Theme.of(context).accentColor,
+            ),
+          ),
         ),
-        title: Text(
-          title,
-          textAlign: TextAlign.center,
-        ),
-        trailing: IconButton(icon: Icon(Icons.shopping_cart), onPressed: () {}),
       ),
     );
   }
