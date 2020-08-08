55100d75bcaa22f0a8d81c6e76ac43f7a713b56c
Nikhil Sharma <nikhil.sk.sharma@gmail.com>
2020-08-08 21:34:19 +0530

    create cart screen

---
+++ lib/screens/cart_screen.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import '../providers/cart.dart' show Cart;
+import '../widgets/cart_item.dart';
+
+class CartScreen extends StatelessWidget {
+  static final routeName = "/cart-screen";
+  @override
+  Widget build(BuildContext context) {
+    final cart = Provider.of<Cart>(context);
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Cart"),
+      ),
+      body: Column(
+        children: <Widget>[
+          Card(
+            margin: EdgeInsets.all(15),
+            child: Padding(
+              padding: const EdgeInsets.all(8),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget>[
+                  Text(
+                    "Total",
+                    style: TextStyle(fontSize: 20),
+                  ),
+                  Spacer(),
+                  Chip(
+                    label: Text(
+                      "Rs. ${cart.totalAmount}",
+                      style: TextStyle(color: Theme.of(context).primaryColor),
+                    ),
+                    backgroundColor: Colors.white,
+                    shadowColor: Theme.of(context).primaryColor,
+                    elevation: 2,
+                  ),
+                  SizedBox(
+                    width: 10,
+                  ),
+                  FlatButton(
+                    onPressed: () {},
+                    child: Text("Order Now"),
+                    color: Theme.of(context).accentColor,
+                  ),
+                ],
+              ),
+            ),
+          ),
+          SizedBox(
+            height: 10,
+          ),
+          Expanded(
+              child: ListView.builder(
+                  itemCount: cart.items.length,
+                  itemBuilder: (ctx, i) => CartItem(
+                    // .values to list is used to convert map to list (map is stored in the cart provider)
+                      id: cart.items.values.toList()[i].id,
+                      title: cart.items.values.toList()[i].title,
+                      quantity: cart.items.values.toList()[i].quantity,
+                      price: cart.items.values.toList()[i].price)))
+        ],
+      ),
+    );
+  }
+}
