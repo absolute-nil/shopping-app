7247555ca4a318f3adce36bafcc6cbaba1e11201
Nikhil Sharma <nikhil.sk.sharma@gmail.com>
2020-08-10 23:56:09 +0530

    add snackbar to undo cart addition

--- lib/providers/cart.dart
+++ lib/providers/cart.dart
@@ -53,13 +53,31 @@ class Cart with ChangeNotifier {
     notifyListeners();
   }
 
-  void removeItem(String productId){
-    _items.remove(productId);
-    notifyListeners();
-  }
-  void clearItems(){
-    _items = {};
+  void removeSingleItem(String productId) {
+    if (!_items.containsKey(productId)) {
+      return;
+    }
+    if (_items[productId].quantity > 1) {
+      _items.update(
+          productId,
+          (product) => CartItem(
+              id: product.id,
+              title: product.title,
+              quantity: product.quantity - 1,
+              price: product.price));
+    } else {
+      _items.remove(productId);
+    }
     notifyListeners();
   }
 
+  void removeItem(String productId) {
+    _items.remove(productId);
+    notifyListeners();
+  }
+
+  void clearItems() {
+    _items = {};
+    notifyListeners();
+  }
 }
--- lib/widgets/product_item.dart
+++ lib/widgets/product_item.dart
@@ -71,6 +71,16 @@ class ProductItem extends StatelessWidget {
               icon: Icon(Icons.shopping_cart),
               onPressed: () {
                 cart.addItem(product.id, product.title, product.price);
+                Scaffold.of(context).removeCurrentSnackBar();
+                Scaffold.of(context).showSnackBar(
+                  SnackBar(
+                    content: Text("${product.title} added to cart"),
+                    duration: Duration(seconds: 2),
+                    action: SnackBarAction(
+                        label: "UNDO",
+                        onPressed: () => cart.removeSingleItem(product.id)),
+                  ),
+                );
               },
               color: Theme.of(context).accentColor,
             ),
