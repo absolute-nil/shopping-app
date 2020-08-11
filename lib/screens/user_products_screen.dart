84ab3a8777c1671f88a2513185b414c091351037
Nikhil Sharma <nikhil.sk.sharma@gmail.com>
2020-08-11 11:02:54 +0530

    create user products screen

---
+++ lib/screens/user_products_screen.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import '../widgets/app_drawer.dart';
+import '../widgets/user_product_item.dart';
+import '../providers/products.dart';
+
+class UserProductsScreen extends StatelessWidget {
+  static const route_name = "/user-products";
+  @override
+  Widget build(BuildContext context) {
+    final products = Provider.of<Products>(context);
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Your products"),
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(Icons.add),
+          )
+        ],
+      ),
+      body: ListView.builder(
+        itemBuilder: (_, i) => UserProductItem(
+          title: products.items[i].title,
+          imageUrl: products.items[i].imageUrl,
+          key: ValueKey(products.items[i].id),
+        ),
+        itemCount: products.items.length,
+      ),
+      drawer: AppDrawer(),
+    );
+  }
+}
