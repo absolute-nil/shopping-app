b200b9f3a0be2c72be1966b31cc23f5136f2b174
Nikhil Sharma <nikhil.sk.sharma@gmail.com>
2020-08-12 21:56:32 +0530

    add fetch and post functionality

--- lib/providers/orders.dart
+++ lib/providers/orders.dart
@@ -1,7 +1,11 @@
+import 'dart:convert';
+
 import 'package:flutter/foundation.dart';
 import 'package:provider/provider.dart';
+import 'package:http/http.dart' as http;
 
 import 'package:intl/intl.dart';
+import 'package:shopping_app/models/http_expecption.dart';
 
 import './cart.dart';
 
@@ -19,20 +23,76 @@ class OrderItem {
 }
 
 class Orders with ChangeNotifier {
+  static const baseUrl = "https://shopping-app-70e63.firebaseio.com";
+
   List<OrderItem> _orders = [];
 
   List<OrderItem> get orders {
     return [..._orders];
   }
 
-  void addOrder(List<CartItem> cartProducts, double total) {
-    _orders.insert(
-        0,
-        OrderItem(
-            amount: total,
-            dateTime: DateTime.now(),
-            id: DateTime.now().toString(),
-            products: cartProducts));
-            notifyListeners();
+  Future<void> fetchAndSetOrders() async {
+    final url = "$baseUrl/orders.json";
+    try {
+      final response = await http.get(url);
+      final decodedResponse =
+          json.decode(response.body) as Map<String, dynamic>;
+      final List<OrderItem> loadedOrders = [];
+      if(decodedResponse == null){
+        return;
+      }
+      decodedResponse.forEach((orderId, orderdata) {
+        loadedOrders.insert(
+            0,
+            OrderItem(
+                id: orderId,
+                amount: orderdata['amount'],
+                dateTime: DateTime.parse(orderdata['dateTime']),
+                products: (orderdata['products'] as List<dynamic>)
+                    .map((item) => CartItem(
+                        id: item['id'],
+                        title: item['title'],
+                        quantity: item['quantity'],
+                        price: item['price']))
+                    .toList()));
+      });
+      _orders = loadedOrders;
+      notifyListeners();
+    } catch (e) {
+      print(e.toString());
+    }
+  }
+
+  Future<void> addOrder(List<CartItem> cartProducts, double total) async {
+    try {
+      final time = DateTime.now();
+      final response = await http.post("$baseUrl/orders.json",
+          body: json.encode({
+            'amount': total,
+            'dateTime': time.toIso8601String(),
+            'products': cartProducts
+                .map((cp) => {
+                      'id': cp.id,
+                      'title': cp.title,
+                      'quantity': cp.quantity,
+                      'price': cp.price
+                    })
+                .toList()
+          }));
+      if (response.statusCode >= 400) {
+        throw HttpException("Could not process order");
+      }
+      _orders.insert(
+          0,
+          OrderItem(
+              amount: total,
+              dateTime: time,
+              id: json.decode(response.body)['name'],
+              products: cartProducts));
+      notifyListeners();
+    } catch (e) {
+      print(e.toString());
+      throw e;
+    }
   }
 }
