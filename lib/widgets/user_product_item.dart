c63c60cb31d42ef993a8c58ef1c02ae5c7cc75fe
Nikhil Sharma <nikhil.sk.sharma@gmail.com>
2020-08-12 14:01:48 +0530

    add delete and edit functionality

--- lib/widgets/user_product_item.dart
+++ lib/widgets/user_product_item.dart
@@ -1,30 +1,69 @@
 import 'package:flutter/cupertino.dart';
-
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import '../screens/product_form_screen.dart';
+import '../providers/products.dart';
 
 class UserProductItem extends StatelessWidget {
-
-
+  final String id;
   final String title;
   final String imageUrl;
 
-  const UserProductItem({Key key, this.title, this.imageUrl}) : super(key: key);
+  const UserProductItem({Key key, this.id, this.title, this.imageUrl})
+      : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Card(
       shadowColor: Theme.of(context).primaryColor,
-          child: ListTile(
+      child: ListTile(
         title: Text(title),
         leading: CircleAvatar(
           backgroundImage: NetworkImage(imageUrl),
         ),
         trailing: Container(
           width: 100,
-          child: Row(children: [
-            IconButton(icon: Icon(Icons.edit),onPressed: (){},color: Theme.of(context).accentColor,),
-            IconButton(icon: Icon(Icons.delete),onPressed: (){},color: Theme.of(context).errorColor,)
-          ],),
+          child: Row(
+            children: [
+              IconButton(
+                icon: Icon(Icons.edit),
+                onPressed: () {
+                  Navigator.of(context)
+                      .pushNamed(ProductFormScreen.route_name, arguments: id);
+                },
+                color: Theme.of(context).accentColor,
+              ),
+              IconButton(
+                icon: Icon(Icons.delete),
+                onPressed: () {
+                  showDialog(
+                      context: context,
+                      builder: (ctx) => AlertDialog(
+                            title: Text("Are you sure?"),
+                            content: Text("Do you want to delete this item?"),
+                            actions: <Widget>[
+                              FlatButton(
+                                  onPressed: () {
+                                    Navigator.of(context).pop();
+                                  },
+                                  child: Text("NO")),
+                              FlatButton(
+                                  onPressed: () {
+                                    Provider.of<Products>(context,
+                                            listen: false)
+                                        .deleteProduct(id);
+
+                                    Navigator.of(context).pop();
+                                  },
+                                  child: Text("YES"))
+                            ],
+                          ));
+                },
+                color: Theme.of(context).errorColor,
+              )
+            ],
+          ),
         ),
       ),
     );
