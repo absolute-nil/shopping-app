c6f82ae4167e4436db90e428b1deb5faf5aa2747
Nikhil Sharma <nikhil.sk.sharma@gmail.com>
2020-08-13 21:03:27 +0530

    link auth provider and state

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
-
+import './providers/auth.dart';
 import './screens/product_form_screen.dart';
 import './screens/product_detail_screen.dart';
 import './screens/product_overview.dart';
@@ -12,6 +12,7 @@ import './screens/cart_screen.dart';
 import './providers/orders.dart';
 import './screens/orders_screen.dart';
 import './screens/user_products_screen.dart';
+import './screens/auth_screen.dart';
 
 void main() => runApp(MyApp());
 
@@ -19,30 +20,36 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MultiProvider(
-      providers: [
-        ChangeNotifierProvider(create: (ctx) => Products()),
-        ChangeNotifierProvider(
-          create: (ctx) => Cart(),
-        ),
-        ChangeNotifierProvider(
-          create: (ctx) => Orders(),
-        )
-      ],
-      child: MaterialApp(
-        title: 'MyShop',
-        theme: ThemeData(
-            primarySwatch: Colors.pink,
-            accentColor: Colors.amberAccent,
-            fontFamily: GoogleFonts.lato().fontFamily),
-        home: ProductsOverview(),
-        routes: {
-          ProductDetailScreen.routeName: (ctx) => ProductDetailScreen(),
-          CartScreen.routeName: (ctx) => CartScreen(),
-          OrdersScreen.route_name: (ctx) => OrdersScreen(),
-          UserProductsScreen.route_name: (ctx) => UserProductsScreen(),
-          ProductFormScreen.route_name: (ctx) => ProductFormScreen()
-        },
-      ),
-    );
+        providers: [
+          ChangeNotifierProvider(create: (ctx) => Auth()),
+          ChangeNotifierProxyProvider<Auth, Products>(
+              create: (_) => Products(null, null, []),
+              update: (ctx, auth, previousProducts) => Products(auth.token, auth.userId,
+                  previousProducts == null ? [] : previousProducts.items)),
+          ChangeNotifierProvider(
+            create: (ctx) => Cart(),
+          ),
+          ChangeNotifierProxyProvider<Auth, Orders>(
+              create: (_) => Orders(null, []),
+              update: (ctx, auth, previousorders) => Orders(auth.token,
+                  previousorders == null ? [] : previousorders.orders)),
+        ],
+        child: Consumer<Auth>(
+          builder: (ctx, auth, _) => MaterialApp(
+            title: 'MyShop',
+            theme: ThemeData(
+                primarySwatch: Colors.pink,
+                accentColor: Colors.amberAccent,
+                fontFamily: GoogleFonts.lato().fontFamily),
+            home: auth.isAuth ? ProductsOverview() : AuthScreen(),
+            routes: {
+              ProductDetailScreen.routeName: (ctx) => ProductDetailScreen(),
+              CartScreen.routeName: (ctx) => CartScreen(),
+              OrdersScreen.route_name: (ctx) => OrdersScreen(),
+              UserProductsScreen.route_name: (ctx) => UserProductsScreen(),
+              ProductFormScreen.route_name: (ctx) => ProductFormScreen()
+            },
+          ),
+        ));
   }
 }
