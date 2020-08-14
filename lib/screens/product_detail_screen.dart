5160a0791caf68e83a49d49ed0b2a9e68e743113
Nikhil Sharma <nikhil.sk.sharma@gmail.com>
2020-08-15 04:04:15 +0530

    add sliver animation

--- lib/screens/product_detail_screen.dart
+++ lib/screens/product_detail_screen.dart
@@ -3,32 +3,89 @@ import 'package:provider/provider.dart';
 
 import '../providers/products.dart';
 
-class ProductDetailScreen extends StatelessWidget {
+class ProductDetailScreen extends StatefulWidget {
   static const routeName = "/product-detail";
+
+  @override
+  _ProductDetailScreenState createState() => _ProductDetailScreenState();
+}
+
+class _ProductDetailScreenState extends State<ProductDetailScreen> {
   @override
   Widget build(BuildContext context) {
     final productId = ModalRoute.of(context).settings.arguments as String;
     final loadedProduct =
         Provider.of<Products>(context, listen: false).findById(productId);
+    var top = 0.0;
+    Widget _leading() {
+      if (top != (MediaQuery.of(context).padding.top + kToolbarHeight))
+        return BackButton(color: Colors.black);
+      return null;
+    }
+
     return Scaffold(
-      appBar: AppBar(title: Text(loadedProduct.title)),
-      body: SingleChildScrollView(
-        child: Column(
-          children: <Widget>[
-            Container(
-              height: 300,
-              width: double.infinity,
-              child: Image.network(loadedProduct.imageUrl,fit: BoxFit.cover,),
+      // appBar: AppBar(title: ),
+      body: CustomScrollView(
+        slivers: [
+          SliverAppBar(
+            expandedHeight: 300,
+            pinned: true,
+            // leading: _leading(),
+            flexibleSpace: LayoutBuilder(
+              builder: (BuildContext context, BoxConstraints constraints) {
+                top = constraints.biggest.height;
+                return FlexibleSpaceBar(
+                  title: Container(
+                    width: double.infinity,
+                    margin: EdgeInsets.only(right: 50),
+                    color: top !=
+                            (MediaQuery.of(context).padding.top +
+                                kToolbarHeight)
+                        ? Colors.black54
+                        : Colors.transparent,
+                    child: Padding(
+                      padding: const EdgeInsets.only(left: 8.0),
+                      child: Text(loadedProduct.title, textAlign: TextAlign.center,),
+                    ),
+                  ),
+                  background: Hero(
+                      tag: loadedProduct.id,
+                      child: Image.network(
+                        loadedProduct.imageUrl,
+                        fit: BoxFit.cover,
+                      )),
+                );
+              },
             ),
-            SizedBox(height: 10,),
-            Text("Rs. ${loadedProduct.price}",style: TextStyle(color: Colors.grey,fontSize: 25),),
-            SizedBox(height: 10,),
+          ),
+          SliverList(
+              delegate: SliverChildListDelegate([
+            SizedBox(
+              height: 10,
+            ),
+            Center(
+              child: Text(
+                "Rs. ${loadedProduct.price}",
+                style: TextStyle(color: Colors.grey, fontSize: 25),
+              ),
+            ),
+            SizedBox(
+              height: 10,
+            ),
             Container(
-              width: double.infinity,
-              padding: EdgeInsets.symmetric(horizontal: 20),
-              child: Text("${loadedProduct.description}",style: TextStyle(fontSize: 15),softWrap: true,textAlign: TextAlign.center,))
-          ],
-        ),
+                width: double.infinity,
+                padding: EdgeInsets.symmetric(horizontal: 20),
+                child: Text(
+                  "${loadedProduct.description}",
+                  style: TextStyle(fontSize: 15),
+                  softWrap: true,
+                  textAlign: TextAlign.center,
+                )),
+            SizedBox(
+              height: 800,
+            )
+          ]))
+        ],
       ),
     );
   }
