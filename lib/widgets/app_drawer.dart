79d2daf3ef7e672b4740a0d4f794a2ef76b189fe
Nikhil Sharma <nikhil.sk.sharma@gmail.com>
2020-08-15 04:05:42 +0530

    use cutom slide transition for orders

--- lib/widgets/app_drawer.dart
+++ lib/widgets/app_drawer.dart
@@ -4,6 +4,7 @@ import 'package:provider/provider.dart';
 import '../screens/user_products_screen.dart';
 import '../screens/orders_screen.dart';
 import '../providers/auth.dart';
+import '../helpers/custom_slide_route.dart';
 
 class AppDrawer extends StatelessWidget {
   @override
@@ -28,7 +29,8 @@ class AppDrawer extends StatelessWidget {
             leading: Icon(Icons.payment),
             title: Text("Orders"),
             onTap: (){
-              Navigator.of(context).pushReplacementNamed(OrdersScreen.route_name);
+              //Navigator.of(context).pushReplacementNamed(OrdersScreen.route_name);
+              Navigator.of(context).pushReplacement(CustomSlideRoute(builder: (ctx) => OrdersScreen()));
             },
           ),
           Divider(),
