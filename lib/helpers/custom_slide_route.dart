4e0977abe6a7c83303f203c03f6fb18ac65210e2
Nikhil Sharma <nikhil.sk.sharma@gmail.com>
2020-08-15 04:03:21 +0530

    create slide transition route

---
+++ lib/helpers/custom_slide_route.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class CustomSlideRoute<T> extends MaterialPageRoute<T> {
+  CustomSlideRoute({WidgetBuilder builder, RouteSettings settings})
+      : super(builder: builder, settings: settings);
+
+  @override
+  Widget buildTransitions(BuildContext context, Animation<double> animation,
+      Animation<double> secondaryAnimation, Widget child) {
+    if (settings.name == "/") {
+      return child;
+    }
+    Animation<Offset> custom = Tween<Offset>(begin: Offset(-1.0, 0.0), end: Offset(0.0, 0.0)).animate(animation);
+    return SlideTransition(position: custom, child: child);
+  }
+}
